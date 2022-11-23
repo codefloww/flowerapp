51533d9f5e0d4a5256a156519a46e792c7b3bdda
codefloww <kryvpaul@gmail.com>
2022-11-23 22:43:30 +0200

    added different mapping

--- src/main/java/ua/edu/ucu/apps/flowerapp/Store.java
+++ src/main/java/ua/edu/ucu/apps/flowerapp/Store.java
@@ -1,5 +1,6 @@
 package ua.edu.ucu.apps.flowerapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,16 @@ import java.util.List;
 @RestController
 public class Store {
     private final ArrayList<Item> assortement = new ArrayList<>();
-
     @GetMapping
+    public final String greeting(){
+        return "Our initial page is working.";
+    }
+    @GetMapping("/all")
     public List<Item> getAssortement() {
-        assortement.add(new Tulip());
         return assortement;
     }
 
-
+    @GetMapping("/search")
     public boolean search(final Item searchedItem) {
         for (Item item : assortement) {
             if (item.equals(searchedItem)) {
@@ -26,7 +29,7 @@ public class Store {
         }
         return false;
     }
-
+    @GetMapping("/add")
     public void addItem(final Item newItem) {
         assortement.add(newItem);
     }
